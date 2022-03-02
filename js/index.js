15189ac0c24cf8e8d0b76cb05adcebc299cde6ba
coderowson <runurowson@gmail.com>
2022-03-02 06:47:39 +0600

    load more

--- js/index.js
+++ js/index.js
@@ -20,20 +20,19 @@ const displayPhones = (phoneList) => {
     // clear data
     phones.textContent = '';
     const first20Data = phoneList.slice(0, 20)
-
     if (first20Data <= 0) {
         return alert('Sorry, no result')
+
     }
     first20Data.forEach(phone => {
         console.log(phone)
         const div = document.createElement('div');
-
         div.classList.add('col-lg-4')
         div.classList.add('mb-5')
         // div.className = "mb-5"
         div.innerHTML = `
-
-        <div  class="card p-3 " style="width: 4rem, background-color: rgb(248, 242, 252);">
+        
+        <div   class="card p-3 " style="width: 4rem, background-color: rgb(248, 242, 252);">
             <img  src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h2 style="color: indigo;" class="card-title">${phone.phone_name}</h2>
@@ -45,9 +44,10 @@ const displayPhones = (phoneList) => {
            
         </div>
         
-       
         `;
+        document.querySelector("#full-phonelist").innerHTML = "See More>>"
         phones.appendChild(div)
+
         // console.log(phone.phoneDetails)
     });
 }
@@ -81,29 +81,51 @@ const displayPhoneDetails = details => {
                 <p><b> Bluetooth:</b> ${details.others.Bluetooth}</p>
                 <p><b> USB:</b> ${details.others.USB}</p>
                 <p><b> GPS:</b> ${details.others.GPS}</p>
-                <p><b> WLAN:</b> ${details.others.WLAN}</p>      
+                <p><b> WLAN:</b> ${details.others.WLAN}</p>
+                      
     </div>
     `
     phoneDetails.appendChild(div)
 }
 
-// const fullPhonelist = () => {
-//     const allphones = document.getElementById("full-phonelist");
-//     const showMore = allphones.innerText;
-//     console.log(showMore)
-//     const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displayAllPhones(data.data))
-//     console.log(url)
-// }
+const fullPhonelist = () => {
+    const allphones = document.getElementById("full-phonelist");
+    const showMore = allphones.innerText;
+    console.log(showMore)
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayAllPhones(data.data))
+    console.log(url)
+}
 
-// const displayAllPhones = allPhoneList => {
-//     const allPhones = document.createElement("div");
-//     console.log(allPhones)
-//     // clear data
-//     allPhones.textContent = '';
-//     div.classList.add('col-lg-4')
-//     div.classList.add('mb-5')
+const displayAllPhones = allPhoneList => {
 
-// }
+    const showAllPhones = document.getElementById("display-allPhone");
+    console.log(showAllPhones)
+    // clear data
+    showAllPhones.textContent = '';
+    for (let phones of allPhoneList) {
+        const div = document.createElement("div");
+        div.classList.add('col-lg-4')
+        div.classList.add('mb-5')
+
+        div.innerHTML = `
+        
+        <div   class="card p-3 " style="width: 4rem, background-color: rgb(248, 242, 252);">
+            <img  src="${phones.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 style="color: indigo;" class="card-title">${phones.phone_name}</h2>
+                <h5 class="card-title"> Brand : ${phones.brand}</h5>
+                
+                <a href="#" onclick="pressPhoneDetails('${phones.slug}')" style="background-color: indigo;" class="btn btn-outline-light border border-rounded fw-bold">Details</a>
+             
+            </div>
+           
+        </div>
+        
+        `;
+        showAllPhones.appendChild(div)
+    }
+
+}
